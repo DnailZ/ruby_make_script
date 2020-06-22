1c2f663b1f7a58a7f386242d66524286d8d94182
dnailz <dnailzb@outlook.com>
2020-06-22 23:16:11 +0800

    autosave

--- test/ruby_make_script_test.rb
+++ test/ruby_make_script_test.rb
@@ -1,11 +1,7 @@
 require "test_helper"
 
 class RubyMakeScriptTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::RubyMakeScript::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
+    def test
+        ~ "pwd"
+    end
 end
