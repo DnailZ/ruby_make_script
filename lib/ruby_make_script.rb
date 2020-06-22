d845c501096271d3896c899c2ae3b343688fe824
dnailz <dnailzb@outlook.com>
2020-06-22 23:24:20 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -24,7 +24,7 @@ end
 def file_modified?(file)
     if $file_target_dict[file].class == FileTarget
         # 文件真正被修改：文件之前不存在，或文件现在已经不存在，或时间戳修改
-        real_modified = $file_time_dict[file] or !File.exists(file) or ($file_time_dict[file] != File.mtime(file))
+        return $file_time_dict[file] or !File.exists(file) or ($file_time_dict[file] != File.mtime(file))
         # 文件依赖被修改
         return real_modified or $file_target_dict[file].depend_modified?
     elsif $file_target_dict[file].class == PhonyTarget
