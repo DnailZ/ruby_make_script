d8f3bef7a661b9e7fc89c372f3595842d5369b32
dnailz <dnailzb@outlook.com>
2020-06-23 00:48:29 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -157,7 +157,6 @@ def make
 
     throw "at least a target" if $targetlist.length < 1
     
-    p $file_time_dict
     if File.exists?('./.make_script.yaml')
         p $file_time_dict
         $file_time_dict = YAML.load(File.read('./.make_script.yaml'))
