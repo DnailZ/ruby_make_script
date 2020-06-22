3e9f5c090c1f5c2ee782208eb1e0fa7e9f841a5f
dnailz <dnailzb@outlook.com>
2020-06-23 00:11:31 +0800

    autosave

--- test/ruby_make_script_test.rb
+++ test/ruby_make_script_test.rb
@@ -5,10 +5,19 @@ class RubyMakeScriptTest < Minitest::Test
         ~ "pwd"
         ~ "ls"
     end
+    def make_file
+        make do
+            "run".phony < ["test.c"] {
+                ~ "echo run"
+            }
+            ["test.c"] < ["test2.c"] {
+                ~ "echo test.c"
+            }
+        end
+    end
     def test_make
         ~ "cd ./test/test_project"
-        make do
-            [""]
-        end
+
+        
     end
 end
