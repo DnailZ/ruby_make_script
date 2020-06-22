b54f71639a86251d9702f1bfeb553e814d5d3650
dnailz <dnailzb@outlook.com>
2020-06-22 23:56:41 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -8,7 +8,7 @@ $cur_file_time_dict = Hash[]
 
 class String
     def ~@
-        puts pastel.red("ruby_make_script>") + self
+        puts pastel.red("ruby_make_script> ") + self
         p system(self)
     end
 end
