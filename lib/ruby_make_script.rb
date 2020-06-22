ece538d501526b277d3da635070de961f93dccbb
dnailz <dnailzb@outlook.com>
2020-06-23 00:45:57 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -36,7 +36,7 @@ def file_modified?(file)
         return real_modified || $file_target_dict[file].depend_modified?
     elsif $file_target_dict[file].class == PhonyTarget
         # 假目标被修改：依赖被修改或之前不存在
-        p "#{$file_time_dict}, #{$file_target_dict}"
+        p $file_time_dict}, #{$file_target_dict}
         return $file_time_dict[file] == nil || $file_target_dict[file].depend_modified?
     else
         throw "file type error #{file.class}"
