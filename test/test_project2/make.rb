bff39fe411a785e1deff5300795cb8c5cf6393f7
dnailz <dnailzb@outlook.com>
2020-06-23 15:34:54 +0800

    autosave

--- test/test_project2/make.rb
+++ test/test_project2/make.rb
@@ -13,11 +13,11 @@ make do
     :app .from "prog" do
         runfile $d[0]
     end
-    "prog" .from (*objects) do
+    "prog" .from(*objects) do
         CC "-o", $t[0], *$d
     end
     sources.zip(objects).each do |s, o|
-        o .from s, (*headers) do
+        o .from(s, *headers) do
             CC "-c", "-o", $t[0], $d[0]
         end
     end
