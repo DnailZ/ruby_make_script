f005d21023f24505b74f70bec38618472b40dff6
dnailz <dnailzb@outlook.com>
2020-06-23 12:06:04 +0800

    autosave

---
+++ test/test_project2/make.rb
@@ -0,0 +1,18 @@
+mkdir? ".build"
+sources = Dir.glob("**/*.c")
+objects = sources.map{ |f| ".build/" + f.gsub('.c', '.o')}
+headers = Dir.glob("**/*.c")
+
+make do
+    :app .from "prog" do
+        runfile $d[0]
+    end
+    "prog" .from *objects do
+        CC "-o", $t[0], *$d
+    end
+    sources.zip(objects).each do |s, o|
+        o .from s, *headers do
+            CC "-c", "-o", $t[0], $d[0]
+        end
+    end
+end
