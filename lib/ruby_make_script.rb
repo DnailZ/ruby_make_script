8378ef82623146e8d53b227b8aa9aaad9e5c8f7c
dnailz <dnailzb@outlook.com>
2020-06-23 19:11:59 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -177,7 +177,7 @@ def make
             puts e.backtrace
         end
     else
-        puts Pastel.new.green.bold("make> ") + "completed"
+        puts Pastel.new.purple.bold("make> ") + "completed"
     end
 
     File.open('./.make_script.yaml', 'w') { |f| f.write(YAML.dump($cur_file_time_dict)) }
