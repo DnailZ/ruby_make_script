922ca7573664ea2de842e460da5dc205f27b9df7
dnailz <dnailzb@outlook.com>
2020-06-23 11:08:01 +0800

    autosave

--- test/ruby_make_script_test.rb
+++ test/ruby_make_script_test.rb
@@ -33,6 +33,7 @@ class RubyMakeScriptTest < Minitest::Test
         make_file1
         check_file("a.out", ".make_script.yaml")
 
+        p run
         run("echo '   ' >> test.c")
         mtime = File.mtime('a.out')
         make_file1
