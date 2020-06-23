8e001348ab9cd60d980938a60302edb0f503f9eb
dnailz <dnailzb@outlook.com>
2020-06-23 12:16:32 +0800

    autosave

--- test/ruby_make_script_test.rb
+++ test/ruby_make_script_test.rb
@@ -7,7 +7,9 @@ class RubyMakeScriptTest < Minitest::Test
     end
 
     def make_file
-        require "./make.rb"
+        fork{
+            require "./make.rb"
+        }
     end
 
     def CC(*str)
