5e5518bcc93314ac1db4cd9152e0e335d8956819
dnailz <dnailzb@outlook.com>
2020-06-23 11:48:11 +0800

    autosave

--- lib/ruby_make_script/utils.rb
+++ lib/ruby_make_script/utils.rb
@@ -20,6 +20,38 @@ def r(*str)
     puts Pastel.new.green("running> ") + str
     if !system(str) 
         puts Pastel.new.red.bold("error> ") + "command error: " + str
-        throw "make command failed"
+        raise "make command failed"
     end
+end
+
+# since ~ "cd <path>" invalid, add a function here
+def cd?(str)
+    begin
+        Dir.chdir(str)
+    rescue => exception
+        puts Pastel.new.red.bold("error> ") + "command error: cd " + str
+        return false
+    end
+    return true
+end
+
+# these were like cd function
+def rm?(*str)
+    r?"rm #{str.join(' ')}"
+end
+
+# these were like cd function
+def mkdir?(*str)
+    r?"mkdir #{str.join(' ')}"
+end
+
+# no error
+def r?(*str)
+    str =  str.join(" ")
+    puts Pastel.new.green("running> ") + str
+    flag = system(str) 
+    if !flag
+        puts Pastel.new.red.bold("error> ") + "command error: " + str
+    end
+    flag
 end
