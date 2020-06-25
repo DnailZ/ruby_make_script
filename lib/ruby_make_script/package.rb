ffdd16381bdc35b8b5f1ad745250dc7a38cb447e
dnailz <dnailzb@outlook.com>
2020-06-25 13:51:19 +0800

    autosave

---
+++ lib/ruby_make_script/package.rb
@@ -0,0 +1,14 @@
+$apt_pack = Hash["uninstall" => "remove"]
+$brew_pack = Hash["remove" => "uninstall"]
+
+def PACK(cmd, *args)
+    cmd = String(cmd)
+    if `uname -a`['Ubuntu']
+        if $apt_pack[cmd]
+            cmd = $apt_pack[cmd]
+        end
+        r "sudo", "apt", cmd, *args.map{|s| String(s)}
+    elsif `uname -a`['Darwin']
+        r "brew", cmd, *args.map{|s| String(s)}
+    end
+end
