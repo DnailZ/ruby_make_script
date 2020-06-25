9d2ec5903559c1ae0cfa2a2be2a6fecf349699ce
dnailz <dnailzb@outlook.com>
2020-06-25 13:08:41 +0800

    autosave

--- lib/ruby_make_script.rb
+++ lib/ruby_make_script.rb
@@ -193,7 +193,9 @@ def dump_md(filename)
     File.open(filename, 'w') do |f|
         f << "# `#{thisfile}` Usage"
         f << ""
-        yield f
+        if block_given?
+            yield f
+        end
         f << ""
         $targetlist.each { |t|
             if t.class == PhonyTarget
---
+++ test/test_project1/make_usage.md
@@ -0,0 +1,1 @@
+# `make.rb` Usage
