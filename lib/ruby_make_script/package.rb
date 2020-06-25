131a2986bc92852d66764ad85a6df4d05a70cd07
dnailz <dnailzb@outlook.com>
2020-06-25 13:54:24 +0800

    autosave

--- lib/ruby_make_script/package.rb
+++ lib/ruby_make_script/package.rb
@@ -1,15 +1,21 @@
-$apt_pack = Hash["uninstall" => "remove"]
-$brew_pack = Hash["remove" => "uninstall"]
+
+$system_to_pack = Hash[
+    "Ubuntu" => "apk",
+    "Darwin" => "brew"
+]
+$pack = Hash[]
+
+$pack['apt'] = Hash[
+    "uninstall" => "remove"
+]
+
+$pack['brew'] = Hash[
+    "remove" => "uninstall"
+]
 
 def PACK(cmd, *args)
     cmd = String(cmd)
     if `uname -a`['Ubuntu']
-        if $apt_pack[cmd]
-            cmd = $apt_pack[cmd]
-        end
-        r "sudo", "apt", cmd, *args.map{|s| String(s)}
-    elsif `uname -a`['Darwin']
-        r "brew", cmd, *args.map{|s| String(s)}
-    end
+    $system_to_pack
 end
 
